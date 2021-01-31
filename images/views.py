1ef957b58159c27144acf47ffe588bcfc535f063
Himanshu Vaghela <49830725+HimanshuVaghelaa@users.noreply.github.com>
2021-01-31 16:04:39 +0530

    Used Redis For Storing Item Views

--- images/views.py
+++ images/views.py
@@ -11,6 +11,13 @@ from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
 
+import redis
+from django.conf import settings
+
+# Connect to redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT, db=settings.REDIS_DB)
+
 
 @login_required
 def image_create(request):
@@ -40,7 +47,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    # Increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    # Increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
 
 @ajax_required
@@ -85,3 +96,16 @@ def image_list(request):
     if request.is_ajax():
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # Get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    # Get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+
+    return render(request, 'images/image/ranking.html', {'section': 'images', 'most_viewed': most_viewed})
